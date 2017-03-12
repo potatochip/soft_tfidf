d50c339237828ee965b974a70383ca3c0b70d03b
Aaron <aaron@Aarons-MacBook-Air.local>
2017-03-11 18:28:05 -0800

    doc revision

--- soft_tfidf.py
+++ soft_tfidf.py
@@ -2,8 +2,6 @@
 Improved over the original in a few ways:
 Terms not seen in the corpus are not ignored. Instead appropriate tfidf weight is given to them.
 Close similarity terms use the partner term idf if it is available.
-Additional option to calculate cos sim from the swapped vectors, but this tends to overweight
-duplicate tokens at the moment.
 """
 import bisect
 import operator
